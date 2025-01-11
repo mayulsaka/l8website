e2c11e3bfb7027316ef0938964760d92516acf12
mayulsaka <mayumilhotesakaguchi@yahoo.co.jp>
2025-01-11 19:26:28 +0900

    250111_(TOPのみ作成済み、「製品・サービス紹」のリンクエリアに不備あり)

--- assets/script/main.js
+++ assets/script/main.js
@@ -8,29 +8,27 @@ ham.on('click', function () {
     nav.toggleClass('active');
 });
 $(window).on('resize', function () {
-        ham.removeClass('active');
-        nav.removeClass('active');
+    ham.removeClass('active');
+    nav.removeClass('active');
 });
 
 
 // ---------- スライダー ---------- 
 
 $('.visual').slick({
-  dots: true,
-  slidesToShow: 3,
-  slidesToScroll:3,
-  autoplay: false, 
-  autoplaySpeed: 5000,
-  arrows: false,
-  responsive: [
-    {
-      breakpoint: 767.9, 
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll:1,
-        centerMode:true,  
-        centerPadding:"8%", 
-      },
-    }, 
-  ],
- });
+    dots: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    autoplay: false,
+    autoplaySpeed: 5000,
+    arrows: false,
+    responsive: [{
+        breakpoint: 767.9,
+        settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            centerMode: true,
+            centerPadding: "8%",
+        },
+    }, ],
+});
