6361072ce7ea982ca33d467a5fdde128fba0353d
mayulsaka <mayumilhotesakaguchi@yahoo.co.jp>
2025-01-11 20:42:47 +0900

    250111v2_(ニュースへ一覧ボタンの動き修正済、「製品・サービス紹」のリンクエリアに不備あり)

--- assets/script/main.js
+++ assets/script/main.js
@@ -15,20 +15,20 @@ $(window).on('resize', function () {
 
 // ---------- スライダー ---------- 
 
-$('.visual').slick({
-    dots: true,
-    slidesToShow: 3,
-    slidesToScroll: 3,
-    autoplay: false,
-    autoplaySpeed: 5000,
-    arrows: false,
-    responsive: [{
-        breakpoint: 767.9,
-        settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            centerMode: true,
-            centerPadding: "8%",
-        },
-    },],
-});
+// $('.visual').slick({
+//     dots: true,
+//     slidesToShow: 3,
+//     slidesToScroll: 3,
+//     autoplay: false,
+//     autoplaySpeed: 5000,
+//     arrows: false,
+//     responsive: [{
+//         breakpoint: 767.9,
+//         settings: {
+//             slidesToShow: 1,
+//             slidesToScroll: 1,
+//             centerMode: true,
+//             centerPadding: "8%",
+//         },
+//     },],
+// });
